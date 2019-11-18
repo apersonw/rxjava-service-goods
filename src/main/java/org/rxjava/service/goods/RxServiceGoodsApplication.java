7e80b4d1418333181bde72046171cddd634d4a95
happy <RJQe1ENAs>
2019-11-18 10:01:44 +0800

    各种修改

--- src/main/java/org/rxjava/service/goods/RxServiceGoodsApplication.java
+++ src/main/java/org/rxjava/service/goods/RxServiceGoodsApplication.java
@@ -1,13 +1,18 @@
 package org.rxjava.service.goods;
 
+import org.rxjava.common.bus.EnableBus;
+import org.rxjava.service.goods.config.GoodsProperties;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author happy 2019-03-17 22:10
  */
 @SpringBootApplication
+@EnableConfigurationProperties(GoodsProperties.class)
+@EnableBus
 public class RxServiceGoodsApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(RxServiceGoodsApplication.class).web(WebApplicationType.REACTIVE).run(args);
