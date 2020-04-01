50f52151b28ff91e542ca84da7007e434ca21b02
happy <wugang@fenglincloud.com>
2020-04-02 01:01:35 +0800

    一些测试性的修改

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mvn clean install -Prelease
 
 # 从pom.xml获取模块名
-module_name=`grep 'artifactId' pom.xml | tr -d '/'| sed 's/<artifactId>//g' | awk 'NR==2 {print}' | sed 's/ //g'`
+module_name=$(grep 'artifactId' pom.xml | tr -d '/' | sed 's/<artifactId>//g' | awk 'NR==2 {print}' | sed 's/ //g')
 # 推送构建的docker镜像到阿里云服务器
 module_tag=registry.cn-shanghai.aliyuncs.com/taro-mall/${module_name}:latest
 
